039ea7858c18bb24a478298639dbe4962869f22e
Liang Hou <houliang.cs@gmail.com>
2022-06-17 23:36:11 +0800

    update scripts

--- BigGAN-PyTorch/scripts/launch_cifar10_ema_tacgan.sh
+++ BigGAN-PyTorch/scripts/launch_cifar10_ema_tacgan.sh
@@ -8,5 +8,5 @@ python train.py \
 --G_attn 0 --D_attn 0 \
 --G_init N02 --D_init N02 \
 --ema --use_ema --ema_start 1000 \
---test_every 1000 --save_every 1000 --num_best_copies 1 --num_save_copies 0 --seed 0 \
+--test_every 2000 --save_every 2000 --num_best_copies 1 --num_save_copies 0 --seed 0 \
 --loss tacgan --G_lambda 1.0 --D_lambda 1.0 --experiment_name c10_tacgan_n4_b50_e1000_a1000_w1
